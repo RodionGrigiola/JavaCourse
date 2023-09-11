4719be4ef902233eb78fa9244efc8f18844cfc83
Rodion Grigiola <rogrigiola@gmail.com>
2023-09-11 13:22:32 +0200

    Finished Problem 2

--- Lab_1/src/Main.java
+++ Lab_1/src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         problem_1();
+        problem_2();
     }
 
     static void problem_1() {
@@ -24,8 +25,30 @@ public class Main {
         }
         System.out.println("Для создания последовательности Коллатца для заданного числа потребовалось " + steps + " шагов.");
     }
+    static void problem_2() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int number = scanner.nextInt();
+        while(number < 0) {
+            System.out.println("Число должно быть больше нуля! Давайте по новой:");
+            number = scanner.nextInt();
+        }
 
-
+        int alternatingSum = 0;
+        boolean toggleOperator = true;
+        for(int i = 0; i < number; i++) {
+            int n = scanner.nextInt();
+            if(toggleOperator) {
+                alternatingSum += n;
+                toggleOperator = false;
+            }
+            else {
+                alternatingSum -= n;
+                toggleOperator = true;
+            }
+        }
+        System.out.println("Сумма знакочередующегося ряда равна " + alternatingSum);
+    }
 
 
 
