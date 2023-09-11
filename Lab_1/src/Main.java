df9a7f798825224db6b2214c83fe97ed79a1a769
Rodion Grigiola <rogrigiola@gmail.com>
2023-09-11 13:00:55 +0200

    Solved Problem 1

--- Lab_1/src/Main.java
+++ Lab_1/src/Main.java
@@ -1,17 +1,37 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        problem_1();
     }
+
+    static void problem_1() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int number = scanner.nextInt();
+        int steps = 0;
+
+        while(true) {
+            if(number % 2 == 0) {
+                number = number / 2;
+            }
+            else {
+                number = number * 3 + 1;
+            }
+            steps++;
+
+            if(number == 1) break;
+        }
+        System.out.println("Для создания последовательности Коллатца для заданного числа потребовалось " + steps + " шагов.");
+    }
+
+
+
+
+
+
+
 }
+
+
+
