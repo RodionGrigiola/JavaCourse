eb157a634b96b0441961a11dc8843beb49265d2e
Rodion Grigiola <rogrigiola@gmail.com>
2023-09-24 16:03:42 +0200

    Finished Problem_2 Lab_2

--- Lab_2/src/Main.java
+++ Lab_2/src/Main.java
@@ -1,17 +1,42 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        // Problem_2
+        int[] arr1 = {1, 3, 5, 7};
+        int[] arr2 = {2, 4, 6, 8};
+        int[] mergedArray = problem_2(arr1, arr2);
+        System.out.println("Массив после слияния:");
+        for (int num : mergedArray) {
+            System.out.print(num + " ");
         }
     }
+
+    static int[] problem_2(int[] arr1, int[] arr2) {
+        int[] mergedArray = new int[arr1.length + arr2.length];
+        int index1 = 0; // Индекс для массива arr1
+        int index2 = 0; // Индекс для массива arr2
+        int mergedIndex = 0; // Индекс для объединенного массива
+
+        while (index1 < arr1.length && index2 < arr2.length) {
+            if (arr1[index1] < arr2[index2]) {
+                mergedArray[mergedIndex++] = arr1[index1++];
+            } else {
+                mergedArray[mergedIndex++] = arr2[index2++];
+            }
+        }
+
+        // Если остались элементы в одном из массивов, добавляем их в объединенный массив
+        while (index1 < arr1.length) {
+            mergedArray[mergedIndex++] = arr1[index1++];
+        }
+
+        while (index2 < arr2.length) {
+            mergedArray[mergedIndex++] = arr2[index2++];
+        }
+
+        return mergedArray;
+    }
+
 }
+
+
